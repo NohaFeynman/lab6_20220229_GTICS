8d51a65678bb531cf86f8eb1f9bd0befe8c6c050
Nilton Condori <a20220229@pucp.edu.pe>
2025-10-02 19:14:54 -0500

    Commit de la creación del proyecto

---
+++ src/main/java/com/example/lab6_20220229_gtics/Lab620220229GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab6_20220229_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab620220229GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab620220229GticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab6_20220229_gtics/Lab620220229GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6_20220229_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab620220229GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
